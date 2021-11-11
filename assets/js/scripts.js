e39703295c056ac144bea25c7e88cd9c0505bbec
sadkebab <44508455+sadkebab@users.noreply.github.com>
2021-11-11 10:50:51 +0100

    trying to fix svg

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,8 +8,8 @@ function mobileMenuClick(x) {
 document.onscroll = function(){
     if(!navigator.userAgentData.mobile){
         offset=(window.pageYOffset/100.0);
-        backgroundParallax='50% calc(50% - '+(offset*3)+'em)';
-        document.getElementById("space-section").style.backgroundPosition=backgroundParallax;
+    //    backgroundParallax='50% calc(50% - '+(offset*3)+'em)';
+    //    document.getElementById("space-section").style.backgroundPosition=backgroundParallax;
     }
     
 
